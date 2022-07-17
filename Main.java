639946247539a0e2638dc75825885dd22f159087
Kraid <kr.salazarp10@gmail.com>
2022-07-17 14:49:51 -0600

    Utilizando git en tarea 1

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe tu nombre");
+        String nombre =  teclado.nextLine();
+        System.out.println("Bienvenido a Progra 2 2022 "+nombre);
+    }
+}
